6f22fcbc535149b5a0845e06f53bf7a4b35f913d
Martijn <Martijn.vandenBurg@glo-integration.com>
2020-12-30 11:57:45 +0100

    example credentials file

---
+++ src/credentials-example.h
@@ -0,0 +1,21 @@
+ /* WiFi credentials. */
+#define HOSTNAME "co2-sensor.esp8266"
+#define MY_SSID ""
+#define MY_PASSWORD ""
+
+
+/* MQTT configuration */
+#define MQTT_SERVER ""
+#define MQTT_PORT 1883
+#define CONNECTION_ID "co2-sensor-values"
+#define CLIENT_NAME "co2-sensor"
+#define CLIENT_PASSWORD ""
+#define TOPIC "domoticz/in"           // MQTT topic for Domoticz
+#define STATUS_TOPIC "outTopic"
+
+/* Domoticz configuration */
+#define IDX_DEVICE_CO2 
+#define IDX_DEVICE_TEMPHUM 
+
+
+/* END */
